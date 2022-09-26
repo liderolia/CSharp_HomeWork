852c2b619254f99d25cea6b127b99bd27e24c38e
Lider <liderolia@inbox.ru>
2022-09-27 05:00:09 +0700

    add Task 25, 27, 29

---
+++ HomeWork04/Task25/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите натуральное число B: ");
+int B = Convert.ToInt32(Console.ReadLine()); 
+
+if (B < 0)
+{
+    Console.WriteLine("не корректный ввод");
+}
+else
+{
+    Console.WriteLine($"{A}^{B} = {Math.Pow(A, B)}");
+}
+
+
+// Console.Clear();
+// Console.Write("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("Введите натуральное число B: ");
+// int B = Convert.ToInt32(Console.ReadLine()); 
+// if (B < 0)
+// {
+//     Console.WriteLine("не корректный ввод");
+// }
+// else
+// {
+//     int a = A;
+//     int b = B; //ввела переменную для корректного отображения вывода
+//     while( B > 1)
+//     {
+//        A = A*a;
+//        B--;          
+//     } 
+//     Console.WriteLine($"{a}^{b} = {A}");      
+// }
+
---
+++ HomeWork04/Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+int sum = 0;
+if (N<10)
+{
+    sum = N;
+}
+else
+{
+    while (N > 10)
+    {
+        int tmp = N%10;
+        sum = sum + tmp;
+        N = N/10;
+    }   
+    sum =  sum + N;
+}
+Console.WriteLine($"сумма цифр = {sum}");
+
